e9738f74401e518b60fe60792cfa5fc784977020
Muhammad Ramadhan <m.ramadhan@hotmail.co.id>
2021-12-14 17:43:48 +0700

    Add VUE, Convert HTML ke VUE, pokok e VUE

---
+++ kopma-frontend/src/components/ItemCarousel.vue
@@ -0,0 +1,105 @@
+<template>
+    <!-- Item Carousel Section Begin -->
+    <section class="women-banner spad">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12 mt-5 text-left">
+                    <h4>Promo Minggu ini</h4>
+                    <carousel class="product-slider" :items="4" :nav="false" :autoplay="true" :loop="true" :margin="25" :dots="false">
+                        <div class="product-item">
+                            <div class="pi-pic">
+                                <img src="img/products/hit.jpg" alt="" />
+                                <ul>
+                                    <li class="w-icon active">
+                                        <a href="#"><i class="icon_bag_alt"></i></a>
+                                    </li>
+                                    <li class="quick-view"><a href="product.html">+ Quick View</a></li>
+                                </ul>
+                            </div>
+                            <div class="pi-text">
+                                <div class="catagory-name">Obat Nyamuk</div>
+                                <a href="#">
+                                    <h5>HIT Orange 350ml</h5>
+                                </a>
+                                <div class="product-price">
+                                    Rp 24.000
+                                    <span>Rp 25.500</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="product-item">
+                            <div class="pi-pic">
+                                <img src="img/products/minyak.jpg" alt="" />
+                                <ul>
+                                    <li class="w-icon active">
+                                        <a href="#"><i class="icon_bag_alt"></i></a>
+                                    </li>
+                                    <li class="quick-view"><a href="#">+ Quick View</a></li>
+                                </ul>
+                            </div>
+                            <div class="pi-text">
+                                <div class="catagory-name">Minyak Goreng</div>
+                                <a href="#">
+                                    <h5>Filma Minyak Goreng 2L</h5>
+                                </a>
+                                <div class="product-price">Rp 27.000</div>
+                            </div>
+                        </div>
+                        <div class="product-item">
+                            <div class="pi-pic">
+                                <img src="img/products/susu.jpg" alt="" />
+                                <ul>
+                                    <li class="w-icon active">
+                                        <a href="#"><i class="icon_bag_alt"></i></a>
+                                    </li>
+                                    <li class="quick-view"><a href="#">+ Quick View</a></li>
+                                </ul>
+                            </div>
+                            <div class="pi-text">
+                                <div class="catagory-name">Susu</div>
+                                <a href="#">
+                                    <h5>Ultra Milk Full Cream 1L</h5>
+                                </a>
+                                <div class="product-price">
+                                    Rp 16.000
+                                    <span>Rp 16.500</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="product-item">
+                            <div class="pi-pic">
+                                <img src="img/products/mie.jpg" alt="" />
+                                <ul>
+                                    <li class="w-icon active">
+                                        <a href="#"><i class="icon_bag_alt"></i></a>
+                                    </li>
+                                    <li class="quick-view"><a href="product.html">+ Quick View</a></li>
+                                </ul>
+                            </div>
+                            <div class="pi-text">
+                                <div class="catagory-name">Mie Instan</div>
+                                <a href="#">
+                                    <h5>Indomie Goreng</h5>
+                                </a>
+                                <div class="product-price">
+                                    RP 2.300
+                                    <span>Rp 2.700</span>
+                                </div>
+                            </div>
+                        </div>
+                    </carousel>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Item Carousel Section End -->
+</template>
+
+<script>
+import carousel from "vue-owl-carousel";
+
+export default {
+    name: "ItemCarousel",
+    components: { carousel },
+};
+</script>
---
+++ kopma-frontend/src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="home">
+        <HeaderKopma />
+        <HeroKopma />
+        <ItemCarousel />
+        <FooterKopma />
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderKopma from "@/components/HeaderKopma.vue";
+import HeroKopma from "@/components/HeroKopma.vue";
+import ItemCarousel from "@/components/ItemCarousel.vue";
+import FooterKopma from "@/components/FooterKopma.vue";
+
+export default {
+    name: "Home",
+    components: {
+        HeaderKopma,
+        HeroKopma,
+        ItemCarousel,
+        FooterKopma,
+    },
+};
+</script>
